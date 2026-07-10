368176b6cbef227cfb4c1177a4c0336985246ac5
Franco Rodriguez <francototites@gmail.com>
2026-07-10 01:13:25 -0300

    feat: agrego mas validaciones a las palabras

--- src/hooks/useGame.js
+++ src/hooks/useGame.js
@@ -9,10 +9,24 @@ export function useGame() {
     const [gameOver, setGameOver] = useState(false);
 
     async function addWord(word) {
+        word = word.trim().toLowerCase();
+        if (!word) return;
         const exists = await validateWord(word);
         if (!exists) {
             setMessage("La palabra no existe.");
             return;
+        }
+        if (words.includes(word)) {
+        setMessage("La palabra ya fue utilizada.");
+        return;
+        }
+        if (words.length > 0) {
+        const lastWord = words[words.length - 1];
+        const lastLetter = lastWord[lastWord.length - 1];
+            if (word[0] !== lastLetter) {
+                setMessage("La palabra no respeta la cadena.");
+                return;
+            }
         }
         setWords([...words, word]);
         setScore(score + word.length);
--- src/services/wordService.js
+++ src/services/wordService.js
@@ -3,8 +3,6 @@ import axios from "axios";
 const API_URL = "https://word-api-hmlg.vercel.app/api";
 
 export async function validateWord(word) {
-    word = word.trim().toLowerCase();
-    if (!word) return;
     const response = await axios.get(`${API_URL}/validate`, {params: {word}});
     return response.data.exists;
 }
