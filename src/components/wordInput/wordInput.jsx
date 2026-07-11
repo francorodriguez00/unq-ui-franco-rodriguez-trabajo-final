07e5eaeb23c347ea9b5af8eb484c05de372a0c34
Franco Rodriguez <francototites@gmail.com>
2026-07-11 12:16:26 -0300

    refactor: mejoro boton de enviar y puedo enviar con Enter

--- src/components/wordInput/wordInput.jsx
+++ src/components/wordInput/wordInput.jsx
@@ -3,22 +3,23 @@ import { useState } from "react";
 
 function WordInput({ onSubmit }) {
   const [word, setWord] = useState("");
-  const handleSubmit = () => {
+  const handleSubmit = (event) => {
+    event.preventDefault();
     onSubmit(word);
     setWord("");
   };
   return (
-    <div className="word-input">
+    <form className="word-input" onSubmit={handleSubmit}>
       <input
         type="text"
         placeholder="Ingresá una palabra"
         value={word}
         onChange={(e) => setWord(e.target.value)}
       />
-      <button onClick={handleSubmit}>
+      <button type="submit">
         Enviar
       </button>
-    </div>
+    </form>
   );
 }
 
